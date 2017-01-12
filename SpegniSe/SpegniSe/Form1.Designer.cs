9d92b4385c8a77e93077d9a9a43c963d26423cdc
Marco <Marco@DESKTOP>
2017-01-12 23:51:05 +0100

    Update

--- SpegniSe/SpegniSe/Form1.Designer.cs
+++ SpegniSe/SpegniSe/Form1.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -40,25 +44,56 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "SpegniSe";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(98, 95);
+            this.comboBox1.Items.AddRange(new object[] {
+            "Tempo trascorso",
+            "Cambio colore"});
+            this.comboBox1.Location = new System.Drawing.Point(86, 104);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Seleziona Opzione";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(364, 105);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(361, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(637, 401);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +101,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SpegniSe/SpegniSe/Form1.cs
+++ SpegniSe/SpegniSe/Form1.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace SpegniSe
 {
     public partial class Form1 : Form
@@ -15,6 +14,69 @@ namespace SpegniSe
         public Form1()
         {
             InitializeComponent();
+        }
+        private void Spegni()
+        {
+            Console.WriteLine("spento");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Ferma")
+            {
+                timer1.Enabled = false;
+                button1.Text = "SpegniSe";            
+            }
+            else
+            { 
+                timer1.Enabled = true;
+                button1.Text = "Ferma";
+                switch (comboBox1.Text)
+                {
+                    case "Tempo trascorso":
+                        label1.Text = textBox1.Text;
+                        Int32 x;
+                        if (Int32.TryParse(textBox1.Text.Trim(), out x))
+                        {
+                            label1.Text = Convert.ToString(x);
+                        }
+                        else
+                        {
+                            timer1.Enabled = false;
+                            MessageBox.Show("Non Numerico");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "Tempo trascorso":
+                    textBox1.Enabled = true;
+                    textBox1.Visible = true;
+                    break;
+                default:
+                    textBox1.Enabled = false;
+                    textBox1.Visible = false;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Int32 x;
+            x = Convert.ToInt32(label1.Text);
+            x = x - 1;
+            label1.Text = x.ToString();
+            if (x == 0)
+            {
+                Spegni();
+            }
         }
     }
 }
